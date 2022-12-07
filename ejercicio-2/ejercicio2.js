4280d21e1145f5fb4459607285d6e6262a4f0dda
Juan Carlos Romero Dev <juancarlosromerodev@gmail.com>
2022-12-07 08:33:26 -0500

    Agregando una interfaz interactiva al ejercicio 2

--- ejercicio-2/ejercicio2.js
+++ ejercicio-2/ejercicio2.js
@@ -45,39 +45,65 @@ const repetirCadena = (cadena = "", n = 0) =>{
 
 //Main
 
-let opcion = parseInt(prompt(`BIENVENIDO AL MENU
-Ingrese el numero del ejercicio que desee evaluar:
-    1.) CONTADOR DE CARACTERES
-    2.) CORTADOR DE CADENAS
-    3.) DE UNA A VARIAS CADENAS
-    4.) REPETIDOR DE CADENAS
-    5.) SALIR`));
+let key = true;
 
-switch (opcion) {
-    case 1:
-        cadena = prompt(`Ejercicio 1: CONTADOR DE CARACTERES
-                        Ingrese una cadena a evaluar`);
-        contadorCadena(cadena);
-        break;
-    case 2:
-        cadena = prompt(`Ejercicio 2: CORTADOR DE CADENA
-                        Ingrese una cadena a utilizar`);
-        indice = toString(prompt('Ingrese la cantidad de caracteres para la nueva cadena'));
-        recortarCadena(cadena, indice);
-        break;
-    case 3:
-        cadena = prompt(`Ejercicio 3: DE UNA A VARIAS CADENAS
-                        Ingrese una cadena (+2 caracteres) a utilizar`);
-        separador = prompt('Ingrese el caracter separador');
-        arregloSubCadenas("Hola que tal", separador);
-        break;
-    case 4:
-        cadena = prompt(`Ejercicio 4: REPETIDOR DE CADENAS
-                        Ingrese una cadena a utilizar`);
-        nrep = yoString(prompt('Ingresa el numero de repeticiones que habra'));
-        repetirCadena(cadena, nrep);
-        break;
-    default:
-        console.log("Ingrese una de las opciones validas del menu");
-        break;
-}
+do {
+        
+    let opcion = parseInt(prompt(`BIENVENIDO AL MENU
+    Ingrese el numero del ejercicio que desee evaluar:
+        1.) CONTADOR DE CARACTERES
+        2.) CORTADOR DE CADENAS
+        3.) DE UNA A VARIAS CADENAS
+        4.) REPETIDOR DE CADENAS
+        5.) SALIR`));
+
+    switch (opcion) {
+        case 1:
+            cadena = prompt(`Ejercicio 1: CONTADOR DE CARACTERES
+                            Ingrese una cadena a evaluar`);
+            contadorCadena(cadena);
+            break;
+        case 2:
+            cadena = prompt(`Ejercicio 2: CORTADOR DE CADENA
+                            Ingrese una cadena a utilizar`);
+            indice = parseInt(prompt('Ingrese la cantidad de caracteres para la nueva cadena'));
+            recortarCadena(cadena, indice);
+            break;
+        case 3:
+            cadena = prompt(`Ejercicio 3: DE UNA A VARIAS CADENAS
+                            Ingrese una cadena (+2 caracteres) a utilizar`);
+            separador = prompt('Ingrese el caracter separador');
+            arregloSubCadenas(cadena, separador);
+            break;
+        case 4:
+            cadena = prompt(`Ejercicio 4: REPETIDOR DE CADENAS
+                            Ingrese una cadena a utilizar`);
+            nrep = parseInt(prompt('Ingresa el numero de repeticiones que habra'));
+            repetirCadena(cadena, nrep);
+            break;
+        case 5:
+            key = false;
+            break;
+        default:
+            alert("Ingrese una de las opciones validas del menu");
+            break;
+    }
+
+    if(key === true && (opcion>0 && opcion<5)){
+        cadena = prompt(`¿Desea salir del programa?
+        Ingrese S (Si)| N (No)`).toUpperCase().charAt(0);
+
+        if(cadena === 'S'){
+            key = false;
+        } else if (cadena === 'N'){
+
+        }else{
+            console.log("Valor no valido. Se procede a regresar al menu");
+        }
+    }
+    
+    if (key === false){
+        alert("Gracias por revisar el programa :D");
+    }
+
+} while (key === true);
